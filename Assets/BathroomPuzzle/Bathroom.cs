178d9b2b74c050c89720c218b49448c800b26407
Simpolar <Simpolar25@hotmail.com>
2024-09-14 18:42:56 +1200

    adding in more level animation(idle and up)

--- Assets/BathroomPuzzle/Bathroom.cs
+++ Assets/BathroomPuzzle/Bathroom.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Platformer
 {
-    public class Bathroom : MonoBehaviour
+    public class Bathroom : MonoBehaviour, IInteractable
     {
         public GameObject PlayerDead;
         public Collider ElectricColliderTrigger;
@@ -14,17 +14,17 @@ namespace Platformer
 
         public GameObject BathroomDeathCam;
 
-        public Animator animator1, animator2, animator3, animator4, animator5, animator6;
+        public Animator leverAnimator1, leverAnimator2, leverAnimator3, leverAnimator4, leverAnimator5, leverAnimator6;
         public Collider lever1, lever2, lever3, lever4, lever5, lever6;
         public AudioClip leverSoundEffect;
 
+        [SerializeField] private string _prompt = "Pull Lever";
+        private bool _isLeverActivated = false;
+
         private void Start()
         {
-            if (PlayerDead != null)
-                PlayerDead.SetActive(false);
-
-            if (BathroomDeathCam != null)
-                BathroomDeathCam.SetActive(false);
+            PlayerDead.SetActive(false);
+            BathroomDeathCam.SetActive(false);
         }
 
         private void OnTriggerEnter(Collider ElectricColliderTrigger)
@@ -42,5 +42,31 @@ namespace Platformer
             }
         }
 
+        public string InteractionPrompt => _prompt;
+
+        public bool Interact(Interactor interactor)
+        {
+            if (_isLeverActivated) return false;
+            ActivateLever();
+            return true;
+        }
+
+        private void ActivateLever()
+        {
+            _isLeverActivated = true;
+
+            if (leverAnimator1 != null) leverAnimator1.SetTrigger("ActivateLever");
+            if (leverAnimator2 != null) leverAnimator2.SetTrigger("ActivateLever");
+            if (leverAnimator3 != null) leverAnimator3.SetTrigger("ActivateLever");
+            if (leverAnimator4 != null) leverAnimator4.SetTrigger("ActivateLever");
+            if (leverAnimator5 != null) leverAnimator5.SetTrigger("ActivateLever");
+            if (leverAnimator6 != null) leverAnimator6.SetTrigger("ActivateLever");
+
+            if (leverSoundEffect != null)
+            {
+                AudioSource.PlayClipAtPoint(leverSoundEffect, transform.position);
+            }
+
+        }
     }
 }
