521002048d7baa7bb0ae8a6fb9ccb0c2e92143c8
dasharussia <146147842+dasharussia@users.noreply.github.com>
2024-09-19 22:53:05 +1200

    buttons

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Platformer
 {
-    public class MainMenu : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
+    public class MainMenu : MonoBehaviour{
+
+        public void PlayGame ()
         {
-        
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
-        // Update is called once per frame
-        void Update()
+        public void QuitGame ()
         {
-        
+            Debug.Log ("QUIT!");
+            Application.Quit();
         }
     }
 }
